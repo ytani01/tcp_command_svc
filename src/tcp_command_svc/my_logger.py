4fc103b4385d1fe32b63b481b29f1ede91ed6137
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2025-07-23 00:59:52 +0900

    REF: my_logger.py to prevent duplicate log messages
    - Modified  to clear existing handlers before adding new ones.
    - Ensured  and  are created dynamically for each logger instance.

--- src/tcp_command_svc/my_logger.py
+++ src/tcp_command_svc/my_logger.py
@@ -4,22 +4,13 @@
 """
 my_logger.py
 """
-__author__ = 'Yoichi Tanibayashi'
+__author__ = 'Yoichi Tanibayahashi'
 __date__ = '2021'
 
 import inspect
 from logging import getLogger, StreamHandler, Formatter
 from logging import DEBUG, INFO
 # from logging import NOTSET, DEBUG, INFO, WARNING, ERROR, CRITICAL
-
-FMT_HDR = '%(asctime)s %(levelname)s '
-FMT_LOC = '%(name)s.%(funcName)s:%(lineno)d> '
-HANDLER_FMT = Formatter(FMT_HDR + FMT_LOC + '%(message)s',
-                        datefmt='%H:%M:%S')
-
-CONSOLE_HANDLER = StreamHandler()
-CONSOLE_HANDLER.setFormatter(HANDLER_FMT)
-CONSOLE_HANDLER.setLevel(DEBUG)
 
 
 def get_logger(name, debug=False):
@@ -29,9 +20,25 @@ def get_logger(name, debug=False):
     filename = inspect.stack()[1].filename.split('/')[-1]
     name = filename + '.' + name
     logger = getLogger(name)
-    logger.propagate = False
-    logger.addHandler(CONSOLE_HANDLER)
-    logger.setLevel(INFO)
+    logger.propagate = False # Prevent messages from being passed to the root logger
+
+    # Clear existing handlers to prevent duplicates if get_logger is called multiple times for the same name
+    if logger.handlers:
+        logger.handlers.clear()
+
+    # Create a new handler and formatter for each logger
+    fmt_hdr = '%(asctime)s %(levelname)s '
+    fmt_loc = '%(name)s.%(funcName)s:%(lineno)d> '
+    handler_fmt = Formatter(fmt_hdr + fmt_loc + '%(message)s',
+                            datefmt='%H:%M:%S')
+
+    console_handler = StreamHandler()
+    console_handler.setFormatter(handler_fmt)
+    console_handler.setLevel(DEBUG) # Set handler level to DEBUG to allow all messages through
+
+    logger.addHandler(console_handler)
+
+    logger.setLevel(INFO) # Default logger level
 
     # [Important !! ]
     # isinstance()では、boolもintと判定されるので、
@@ -40,7 +47,6 @@ def get_logger(name, debug=False):
     if isinstance(debug, bool):
         if debug:
             logger.setLevel(DEBUG)
-
         return logger
 
     if isinstance(debug, int):
